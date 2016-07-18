fbe87919f363ddf57b0ad7ebdb52132a0365ef96
HarrisLeow <leow_harris@hotmail.sg>
2016-07-18 10:22:57 +0800

    Changed put to patch

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/', function (req, res, next) {
 router.get('/makans', makanController.getAllMakans)
 router.post('/makans', makanController.makeNewMakan)
 router.route('/makans/:id')
-  .put(userController.userLoggedIn, makanController.updateMakan)
+  .patch(userController.userLoggedIn, makanController.updateMakan)
 router.get('/users', userController.getAllUsers)
 router.get('/users/:id', userController.getOneUser)
 
